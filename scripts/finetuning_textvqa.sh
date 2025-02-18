7255572dd240e8100d16ca28c593639fe6045585
FTFWO <166468908+FTFWO@users.noreply.github.com>
2025-02-18 16:08:52 +0800

    Update finetuning_textvqa.sh

--- scripts/finetuning_textvqa.sh
+++ scripts/finetuning_textvqa.sh
@@ -1,1 +1,1 @@
-CUDA_VISIBLE_DEVICES=$1 python tools/run.py --tasks vqa --datasets m4c_textvqa --model CT-HSR --seed $2 --config configs/vqa/m4c_textvqa/refine.yml --save_dir save/$3 --resume_file save/$4/best.ckpt training_parameters.distributed True
+CUDA_VISIBLE_DEVICES=$1 python tools/run.py --tasks vqa --datasets m4c_textvqa --model CT-HSR --seed $2 --config configs/vqa/m4c_textvqa/refine.yml --save_dir save/$3 --resume_file save/$4/best.ckpt 
